2e9223f65da8d4536365750d25cc18c6dbc59b3e
Poolpi Tako <poolpitako@protonmail.com>
2021-10-25 13:39:07 -0300

    feat: added happy path test

--- tests/conftest.py
+++ tests/conftest.py
@@ -78,6 +78,10 @@ token_prices = {
     "AAVE": 300,
 }
 
+@pytest.fixture
+def stkaave():
+    yield Contract("0x4da27a545c0c5B758a6BA100e3a049001de870f5")
+
 
 @pytest.fixture(autouse=True, scope="function")
 def amount(token, token_whale, user):
